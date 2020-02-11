95ea51cac1198abfa356a6da23741756ab98023c
Ying Yi <maggieyi868@gmail.com>
2020-02-11 12:29:06 +0000

    Revert "Fix for <https://github.com/SNSystems/llvm-prepo/issues/10>. This change disables the use of 'constructor aliases' to workaround the repository pruning pass's lack of support for aliases at the moment."
    This reverts commit 746aac914662eee2ded0a189f45c37e337fb8730.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -3948,10 +3948,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
   // Enable -mconstructor-aliases except on darwin, where we have to work around
   // a linker bug (see <rdar://problem/7651567>), and CUDA device code, where
   // aliases aren't supported.
-  // The program repo's digest-generation and pruning passes don't currently
-  // support aliases, so disable them here.
-  if (!RawTriple.isOSDarwin() && !RawTriple.isNVPTX() &&
-      !RawTriple.isOSBinFormatRepo())
+  if (!RawTriple.isOSDarwin() && !RawTriple.isNVPTX())
     CmdArgs.push_back("-mconstructor-aliases");
 
   // Darwin's kernel doesn't support guard variables; just die if we
