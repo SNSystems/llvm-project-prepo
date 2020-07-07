511595847956b5e07c8a58b9f1e2a1b2d697c294
Paul Bowen-Huggett <paulhuggett@mac.com>
2020-07-07 09:43:02 +0200

    issue #101: Upstream changes need to be reflected in our Docker setup.

--- llvm/utils/repo/docker/build.sh
+++ llvm/utils/repo/docker/build.sh
@@ -6,7 +6,7 @@
     --install-target install-pstore \
     --install-target install-repo2obj \
     --install-target install-repo-ticket-dump \
-    --install-target install-clang-headers \
+    --install-target install-clang-resource-headers \
     -- \
     -D CMAKE_BUILD_TYPE=Release \
     -D LLVM_PARALLEL_LINK_JOBS=1
