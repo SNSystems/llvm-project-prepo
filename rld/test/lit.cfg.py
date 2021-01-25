8d7d96223f28088d2984ba82a04704c51140bb62
Paul Bowen-Huggett <paulhuggett@mac.com>
2021-01-25 15:49:08 +0100

    Update to the "new" LLVM license. (#138)

--- rld/test/lit.cfg.py
+++ rld/test/lit.cfg.py
@@ -1,4 +1,16 @@
-# -*- Python -*-
+# *  _ _ _          __        *
+# * | (_) |_   ___ / _| __ _  *
+# * | | | __| / __| |_ / _` | *
+# * | | | |_ | (__|  _| (_| | *
+# * |_|_|\__(_)___|_|  \__, | *
+# *                    |___/  *
+# ===- test/lit.cfg.py ----------------------------------------------------===//
+#
+#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+#  See https://llvm.org/LICENSE.txt for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# ===----------------------------------------------------------------------===//
 
 # Configuration file for the 'lit' test runner.
 
