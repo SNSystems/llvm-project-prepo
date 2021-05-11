9d3d211c833da8e8d3fb2f2a7091eb111239f1c3
Ying Yi <ying.yi@sony.com>
2021-05-11 23:57:24 +0100

    Modify the repo.cmake to support of the musl libc.a and LLVM libc++.a. and add a Makefile to build LLVM runtime libraries using musl libc library.

--- llvm/utils/repo/repo.cmake
+++ llvm/utils/repo/repo.cmake
@@ -15,6 +15,9 @@ set (triple "x86_64-pc-linux-gnu-repo")
 SET (CMAKE_C_COMPILER   "clang"  CACHE FILEPATH "Compiler")
 SET (CMAKE_CXX_COMPILER "clang++"  CACHE FILEPATH "Compiler")
 
+option(libcxx "Build the project using the LLVM libc++ library." OFF)
+option(musl "Build the project using the musl libc library." OFF)
+
 # The user may specify the utils_dir by giving
 # '-Dutils_dir:STRING=/path/to/llvm/utils/repo' on the command line
 if (utils_dir)
@@ -24,10 +27,52 @@ else ()
     set (utils_dir /usr/share/repo)
 endif ()
 
-SET (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -fno-exceptions -fno-rtti" CACHE STRING "Default C Flags Debug")
-SET (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -fno-exceptions -fno-rtti" CACHE STRING "Default CXX Flags Debug")
-SET (CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3 -fno-exceptions -fno-rtti" CACHE STRING "Default C Flags Release")
-SET (CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -fno-exceptions -fno-rtti" CACHE STRING "Default CXX Flags Release")
+# libcxxabi_include: the path of the directory containing the clang's include files.
+# The user may specify the libcxxabi_include by giving
+# '-Dlibcxxabi_include:STRING=/path/to/clang/include' on the command line.
+if (libcxxabi_include)
+	set (libcxxabi_include "${libcxxabi_include}")
+endif ()
+
+# musl_install: the path of the directory in which musl libc is installed.
+# The user may specify the musl_install by giving
+# '-Dmusl_install:STRING=/path/to/musl/installed/directory' on the command line.
+if (musl_install)
+	set (musl_install "${musl_install}")
+else ()
+	set (musl_install "/home/prepo/musl")
+endif ()
+
+# llvm_install: the path of the directory in which llvm libraries are installed.
+# The user may specify the llvm_install by giving
+# '-Dllvm_install:STRING=/path/to/llvm/installed/directory' on the command line.
+if (llvm_install)
+	set (llvm_install "${llvm_install}")
+else ()
+	set (llvm_install "/home/prepo/LLVM")
+endif ()
+
+# The user may use the LLVM libc++.a by giving
+# '-Dlibcxx:BOOL=Yes' on the command line
+if (libcxx)
+	set (libcxx_flags "-stdlib=libc++")
+	set (libcxxabi_lib "-L ${llvm_install}/lib -stdlib=libc++ -lc++abi")
+endif()
+
+# The user may use the MUSL libc.a by giving
+# '-Dmusl:BOOL=Yes' on the command line
+if (musl)
+	SET (musl_compile_flags "-nostdinc -nostdinc++ --sysroot ${musl_install} -isystem ${musl_install}/include ${libcxxabi_include}")
+	SET (CMAKE_EXE_LINKER_FLAGS "-nostdlib -nodefaultlibs -static --sysroot ${musl_install} -L ${musl_install}/lib ${libcxxabi_lib} -lc_elf" CACHE STRING "toolchain_exelinkflags" FORCE)
+endif()
+
+SET (CMAKE_C_FLAGS "${musl_compile_flags} -fno-exceptions -fno-rtti" CACHE STRING "toolchain_cflags")
+SET (CMAKE_CXX_FLAGS "${musl_compile_flags} ${libcxx_flags} -fno-exceptions -fno-rtti" CACHE STRING "toolchain_cxxflags")
+
+SET (CMAKE_C_FLAGS_DEBUG " -O0 " CACHE STRING "Default C Flags Debug")
+SET (CMAKE_CXX_FLAGS_DEBUG " -O0 " CACHE STRING "Default CXX Flags Debug")
+SET (CMAKE_C_FLAGS_RELEASE " -O3 " CACHE STRING "Default C Flags Release")
+SET (CMAKE_CXX_FLAGS_RELEASE " -O3 " CACHE STRING "Default CXX Flags Release")
 set (CMAKE_C_COMPILER_TARGET ${triple})
 set (CMAKE_CXX_COMPILER_TARGET ${triple})
 
@@ -37,7 +82,6 @@ set (CMAKE_CXX_LINKER ${CMAKE_LINKER})
 
 set (CMAKE_C_LINK_EXECUTABLE "${utils_dir}/link.py <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 set (CMAKE_CXX_LINK_EXECUTABLE "${utils_dir}/link.py <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
-
 
 set (CMAKE_C_CREATE_SHARED_LIBRARY   "${utils_dir}/link.py <FLAGS> <CMAKE_SHARED_LIBRARY_C_FLAGS> <LANGUAGE_COMPILE_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS> <SONAME_FLAG><TARGET_SONAME> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
 set (CMAKE_CXX_CREATE_SHARED_LIBRARY "${utils_dir}/link.py <FLAGS> <CMAKE_SHARED_LIBRARY_CXX_FLAGS> <LANGUAGE_COMPILE_FLAGS> <LINK_FLAGS> <CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS> <SONAME_FLAG><TARGET_SONAME> -o <TARGET> <OBJECTS> <LINK_LIBRARIES>")
