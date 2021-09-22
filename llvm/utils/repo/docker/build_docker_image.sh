b6f61d0497f65c07adf170c2f392abbfcf7bf576
Paul Bowen-Huggett <paulhuggett@mac.com>
2021-09-22 22:36:32 +0200

    Copy link++ to the build directory. (#161)
    Previously, the link tool would run clang++. This was mostly okay, but
    linked additional libraries that may not be available on the
    host. Explicitly set the target file format to ELF for linking.

--- llvm/utils/repo/docker/build_docker_image.sh
+++ llvm/utils/repo/docker/build_docker_image.sh
@@ -127,6 +127,7 @@ cp -r "$SOURCE_DIR/scripts" "$BUILD_DIR/scripts"
 mkdir "$BUILD_DIR/repo"
 cp "$SOURCE_DIR/../archive.py"   \
    "$SOURCE_DIR/../link.py"      \
+   "$SOURCE_DIR/../link++.py"      \
    "$SOURCE_DIR/../repo.cmake"   \
    "$SOURCE_DIR/../repo.json"    \
    "$SOURCE_DIR/../wrap_tool.py" \
