98a21a01ecc51442422fe7827e3d016c70ced58a
Paul Bowen-Huggett <paulhuggett@mac.com>
2021-09-22 17:25:42 +0200

    Add the link++ tool for linking C++ programs.
    Previously, the link tool would run clang++. This was mostly okay, but
    linked additional libraries that may not be available on the
    host. Explicitly set the target file format to ELF for linking.

--- llvm/utils/repo/link++.py
+++ llvm/utils/repo/link++.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# ===- link.py - ---------------------------------------------*- python -*--===#
+# ===- link++.py - -------------------------------------------*- python -*--===#
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 # See https://llvm.org/LICENSE.txt for license information.
@@ -19,10 +19,11 @@ import wrap_tool
 def main (argv):
     try:
         logging.basicConfig ()
-        return wrap_tool.wrap_tool (tool='link', argv=['-target','x86_64-pc-linux-gnu-elf'] + argv[1:])
+        return wrap_tool.wrap_tool (tool='link++', argv=['-target','x86_64-pc-linux-gnu-elf']+argv[1:])
     except subprocess.CalledProcessError:
         return 1
 
 if __name__ == '__main__':
     sys.exit (main (sys.argv))
-# eof: link.py
+
+# eof: link++.py
