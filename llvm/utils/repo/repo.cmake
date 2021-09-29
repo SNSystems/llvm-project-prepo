SNSystems/llvm-project-prepo

commit 2a3e95a23fdad395191c4aaf1077e664301ccecf

Docker: install the repo build helper tools into /usr/share/repo.

llvm/utils/repo/repo.cmake
@@ -15,13 +15,13 @@ set (triple "x86_64-pc-linux-gnu-repo")
 SET (CMAKE_C_COMPILER   "clang"  CACHE FILEPATH "Compiler")
 SET (CMAKE_CXX_COMPILER "clang++"  CACHE FILEPATH "Compiler")
 
-# The user must specify the utils_dir by giving
+# The user may specify the utils_dir by giving
 # '-Dutils_dir:STRING=/path/to/llvm/utils/repo' on the command line
 if (utils_dir)
     # Environment variables are always preserved.
     set(ENV{_utils_dir} "${utils_dir}")
 else ()
-    set(utils_dir "$ENV{_utils_dir}")
+    set (utils_dir /usr/share/repo)
 endif ()
 
 SET (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -fno-exceptions -fno-rtti" CACHE STRING "Default C Flags Debug")

llvm/utils/repo/docker/build_docker_image.sh
@@ -123,6 +123,16 @@ echo "Using a temporary directory for the build: $BUILD_DIR"
 cp -r "$SOURCE_DIR/$IMAGE_SOURCE" "$BUILD_DIR/$IMAGE_SOURCE"
 cp -r "$SOURCE_DIR/scripts" "$BUILD_DIR/scripts"
 
+# Copy the repo build utility tools to the docker build directory.
+mkdir "$BUILD_DIR/repo"
+cp "$SOURCE_DIR/../archive.py"   \
+   "$SOURCE_DIR/../link.py"      \
+   "$SOURCE_DIR/../repo.cmake"   \
+   "$SOURCE_DIR/../repo.json"    \
+   "$SOURCE_DIR/../wrap_tool.py" \
+   "$BUILD_DIR/repo/"
+
+
 if [ "$DOCKER_TAG" != "" ]; then
   DOCKER_TAG=":$DOCKER_TAG"
 fi

llvm/utils/repo/docker/llvm-prepo/Dockerfile
@@ -77,6 +77,11 @@ RUN apt-get install -y --no-install-recommends  sudo
 RUN apt-get install -y binutils libstdc++-8-dev
 RUN rm -rf /var/lib/apt/lists/*
 
+COPY repo/archive.py /usr/share/repo/
+COPY repo/link.py /usr/share/repo/
+COPY repo/repo.cmake /usr/share/repo/
+COPY repo/repo.json /usr/share/repo/
+COPY repo/wrap_tool.py /usr/share/repo/
 
 
 # Create the "prepo" user (password "prepo") with sudo privileges.

llvm/utils/repo/wrap_tool.py
@@ -130,18 +130,30 @@ def _open_config_file(name='repo.json'):
     :return: The path of a file with the specified name or None if not found.
     """
 
-    prev_path = None
-    path = os.path.abspath(os.curdir)
-    while path != prev_path:
+    def try_path(p):
         try:
-            fp = open(os.path.join (path, name))
+            fp = open (p)
             _logger.debug('configuration file "%s" was found at "%s"', name, path)
             return fp
         except:
-            pass
+            return None
+
+    # First start in the current directory and work up through the parent
+    # directories.
+    prev_path = None
+    path = os.path.abspath(os.curdir)
+    while path != prev_path:
+        fp = try_path (os.path.join (path, name))
+        if fp:
+            return fp
         # Navigate one level up the directory hierarchy
         prev_path = path
         path = os.path.abspath(os.path.join(path, os.pardir))
+
+    # Try a global location.
+    fp = try_path (os.path.join ('/usr/share/repo', name))
+    if fp:
+        return fp
 
     _logger.warning('configuration file "%s" was not found in any parent directory', name)
     return None

commit 39aaa21a461ff53e86ea4b142d48f845e5f45918

Fix the utils_dir setting.
Setting the directory for the "wrap" utilities was broken by commit
2a3e95a23fdad395191c4aaf1077e664301ccecf which made the switch optional.

llvm/utils/repo/repo.cmake
@@ -18,14 +18,20 @@ SET (CMAKE_CXX_COMPILER "clang++"  CACHE FILEPATH "Compiler")
 option(libcxx "Build the project using the LLVM libc++ library." OFF)
 option(musl "Build the project using the musl libc library." OFF)
 
-# The user may specify the utils_dir by giving
-# '-Dutils_dir:STRING=/path/to/llvm/utils/repo' on the command line
+# The user may specify the path of the "wrap" utilities by passing
+# '-Dutils_dir:STRING=/path/to/llvm/utils/repo' on the command line.
+#
+# We set the '_utils_dir' environment variable to communicate this path to
+# child instances of cmake (during compiler identification, for example).
 if (utils_dir)
-    # Environment variables are always preserved.
-    set(ENV{_utils_dir} "${utils_dir}")
+    set (ENV{_utils_dir} "${utils_dir}")
+elseif (DEFINED ENV{_utils_dir})
+    set (utils_dir "$ENV{_utils_dir}")
 else ()
     set (utils_dir /usr/share/repo)
+    set (ENV{_utils_dir} "${utils_dir}")
 endif ()
+
 
 # libcxxabi_include: the path of the directory containing the clang's include files.
 # The user may specify the libcxxabi_include by giving